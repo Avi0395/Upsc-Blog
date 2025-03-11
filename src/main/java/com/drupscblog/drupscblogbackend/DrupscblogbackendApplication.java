8f230dcfb6be55880ecab8fd423931a15702db69
Avi0395 <avidipakpatil.com>
2025-03-11 20:47:17 +0530

    Initial Commit

---
+++ src/main/java/com/drupscblog/drupscblogbackend/DrupscblogbackendApplication.java
@@ -0,0 +1,13 @@
+package com.drupscblog.drupscblogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrupscblogbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrupscblogbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drupscblog/drupscblogbackend/DrupscblogbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drupscblog.drupscblogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrupscblogbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
